aa1d23101dd17dc1eea7e20ddd4d7a4c458b043e
Elena Chaikouskaya <dolika0ch@gmail.com>
2024-11-12 12:35:11 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+def isCorrectRect(x,y):
+    pass
+
+koord1 = input("Введите координаты левого нижнего угла прямоугольника : ")
+koord2 = input("Введите координаты верхнего правого угла прямоугольника : ")
+isCorrectRect(koord1,koord2)
